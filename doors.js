70e66f8a20feef0236fdb2b6ac8d9d935f7f7e3b
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-12-12 15:25:19 +0100

    add doors.js, create allowedDoors func

---
+++ doors.js
@@ -0,0 +1,16 @@
+const doors = [
+    {
+        date: new Date('2024-12-01T00:00:00'),
+        content: 'This is a fun fact about December 1st.',
+    },
+    {
+        date: new Date('2024-12-12T00:00:00'),
+        content: 'This is a riddle about December 12th.',
+    },
+    {
+        date: new Date('2024-12-18T00:00:00'),
+        content: 'This is a riddle about December 18th.',
+    }
+]
+
+export default doors;
--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+import doors from './doors.js';
+
 // All functions goes here
 
-// testing js to html connection
-function testFunction(){
-    console.log('Test function called');
+
+function allowedDoors(doors){
+    let allowedDoors = []
+    let todaysDoor = {}
+    let today = new Date();
+    today.setHours(0,0,0,0);
+
+    doors.forEach(door => {
+        if (door.date < today){
+            allowedDoors.push(door);
+        } else if (door.date.toDateString() === today.toDateString()){
+            todaysDoor = door;
+        } else {
+            console.log('Error in allowedDays function');
+        }
+    });
+    console.log('Allowed days: ', allowedDoors);
+    console.log('Today\'s day: ', todaysDoor);
 }
 
-testFunction();
+allowedDoors(doors);
