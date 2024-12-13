52fdd0e1e03564040f9f8a4ca2010b3f38aed4b5
KellyT44 <k.thompson4421@yahoo.com>
2024-12-13 10:53:43 +0000

    added all 24doors, content to follow

--- doors.js
+++ doors.js
@@ -8,6 +8,51 @@ const doors = [
         id: "door2",
         date: new Date('2024-12-02T00:00:00'),
         content: 'This is a fun fact about December 2nd.',
+    }
+    {
+        id: "door3",
+        date: new Date('2024-12-03T00:00:00'),
+        content: 'This is a fun fact about December 3rd.',
+    },
+    {
+        id: "door4",
+        date: new Date('2024-12-04T00:00:00'),
+        content: 'This is a fun fact about December 4th.',
+    },
+    {
+        id: "door5",
+        date: new Date('2024-12-05T00:00:00'),
+        content: 'This is a fun fact about December 5th.',
+    },
+    {
+        id: "door6",
+        date: new Date('2024-12-06T00:00:00'),
+        content: 'This is a fun fact about December 6th.',
+    },
+    {
+        id: "door7",
+        date: new Date('2024-12-07T00:00:00'),
+        content: 'This is a fun fact about December 7th.',
+    },
+    {
+        id: "door8",
+        date: new Date('2024-12-08T00:00:00'),
+        content: 'This is a fun fact about December 8th.',
+    },
+    {
+        id: "door9",
+        date: new Date('2024-12-09T00:00:00'),
+        content: 'This is a fun fact about December 9th.',
+    },
+    {
+        id: "door10",
+        date: new Date('2024-12-10T00:00:00'),
+        content: 'This is a fun fact about December 10th.',
+    },
+    {
+        id: "door11",
+        date: new Date('2024-12-11T00:00:00'),
+        content: 'This is a fun fact about December 11th.',
     },
     {
         id: "door12",
@@ -15,10 +60,66 @@ const doors = [
         content: 'This is a riddle about December 12th.',
     },
     {
+        id: "door13",
+        date: new Date('2024-12-13T00:00:00'),
+        content: 'This is a fun fact about December 13th.',
+    },
+    {
+        id: "door14",
+        date: new Date('2024-12-14T00:00:00'),
+        content: 'This is a fun fact about December 14th.',
+    },
+    {
+        id: "door15",
+        date: new Date('2024-12-15T00:00:00'),
+        content: 'This is a fun fact about December 15th.',
+    },
+    {
+        id: "door16",
+        date: new Date('2024-12-16T00:00:00'),
+        content: 'This is a fun fact about December 16th.',
+    },
+    {
+        id: "door17",
+        date: new Date('2024-12-17T00:00:00'),
+        content: 'This is a fun fact about December 17th.',
+    },
+    {
         id: "door18",
         date: new Date('2024-12-18T00:00:00'),
         content: 'This is a riddle about December 18th.',
     }
+    {
+        id: "door19",
+        date: new Date('2024-12-19T00:00:00'),
+        content: 'This is a fun fact about December 19th.',
+    },
+    {
+        id: "door20",
+        date: new Date('2024-12-20T00:00:00'),
+        content: 'This is a fun fact about December 20th.',
+    },
+    {
+        id: "door21",
+        date: new Date('2024-12-21T00:00:00'),
+        content: 'This is a fun fact about December 21st.',
+    },
+    {
+        id: "door22",
+        date: new Date('2024-12-22T00:00:00'),
+        content: 'This is a fun fact about December 22nd.',
+    },
+    {
+        id: "door23",
+        date: new Date('2024-12-23T00:00:00'),
+        content: 'This is a fun fact about December 23rd.',
+    },
+    {
+        id: "door24",
+        date: new Date('2024-12-24T00:00:00'),
+        content: 'This is a fun fact about December 24th.',
+    },
+
 ]
 
 export default doors;
