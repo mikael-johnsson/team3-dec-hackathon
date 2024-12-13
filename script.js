8455f2e73ba82196f70993d11b18793800b9c9c2
Mikael Johnsson <mikaeljohanjohnsson@gmail.com>
2024-12-13 12:03:32 +0100

    add toggle function when clicking door

--- script.js
+++ script.js
@@ -9,9 +9,7 @@ import doors from './doors.js';
  * that are allowed to be opened according to todays date
  */
 function allowedDoors(doors){
-    console.log("all doors: ", doors);
     let allowedDoors = []
-    let todaysDoor = {}
     let today = new Date();
     today.setHours(0,0,0,0);
 
@@ -19,7 +17,7 @@ function allowedDoors(doors){
         if (door.date <= today){
             allowedDoors.push(door);
         } else {
-            console.log('Error in allowedDays function');
+            console.log('This door is locked: ', door.date);
         }
     });
     // Returns an array of door objects that are allowed to be opened
@@ -28,7 +26,6 @@ function allowedDoors(doors){
 
 
 let doorsToOpen = allowedDoors(doors);
-console.log(doorsToOpen);
 
 
 /**
@@ -38,6 +35,7 @@ console.log(doorsToOpen);
  * For each allowed door, this function adds the content to the door
  */
 function addContentToDoors(doors){
+    clickableDoors(doors)
     doors.forEach(door => {
         let targetDoor = document.getElementById(door.id)
         targetDoor.querySelector('p').innerHTML = door.content.text
@@ -48,5 +46,20 @@ function addContentToDoors(doors){
         }
     });
 }
+
+function clickableDoors(doors){
+    // console.log("clickable doors: ", doors);
+    doors.forEach(door => {
+        let targetDoor = document.getElementById(door.id);
+        const children = targetDoor.querySelectorAll('.child');
+        targetDoor.addEventListener('click', (e) => {
+            children.forEach(child => {
+                child.classList.toggle('hidden');
+            });
+            
+        });
+    });
+}
+
 
 addContentToDoors(doorsToOpen);
