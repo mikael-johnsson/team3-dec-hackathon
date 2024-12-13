c23ed0f23b24887376c17e4cd3a19e0f4bb16d3e
Mariam <mar.ayoub98@gmail.com>
2024-12-13 13:38:37 +0400

    create css and js for door flip on click

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ import doors from './doors.js';
  * @returns {array} allowedDoors - an array of all doors
  * that are allowed to be opened according to todays date
  */
-function allowedDoors(doors){
+function allowedDoors(doors) {
     let allowedDoors = []
     let todaysDoor = {}
     let today = new Date();
-    today.setHours(0,0,0,0);
+    today.setHours(0, 0, 0, 0);
 
     doors.forEach(door => {
-        if (door.date < today){
+        if (door.date < today) {
             allowedDoors.push(door);
-        } else if (door.date.toDateString() === today.toDateString()){
+        } else if (door.date.toDateString() === today.toDateString()) {
             todaysDoor = door;
         } else {
             console.log('Error in allowedDays function');
@@ -37,10 +37,28 @@ let doorsToOpen = allowedDoors(doors);
  * returned from allowedDoors function
  * For each allowed door, this function adds the content to the door
  */
-function addContentToDoors(doors){
+function addContentToDoors(doors) {
     doors.forEach(door => {
         document.getElementById(door.id).innerHTML = door.content;
     });
 }
 
 addContentToDoors(doorsToOpen);
+
+/**
+ * Starter function to allow the doors to flip when a user clicks on them.
+ * doorsToFlip can be updated to allowedDoors. This is where we could
+ * check the date and display a message to the user if they try to
+ * open a door when it's not yet that date.
+ */
+function flipDoor(doors) {
+    doors.forEach((door) => {
+        door.addEventListener("click", function () {
+            this.classList.add('card-flip')
+        })
+    });
+};
+;
+
+let doorsToFlip = document.querySelectorAll('.card');
+flipDoor(doorsToFlip);
